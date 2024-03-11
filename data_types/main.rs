be54913671d4c16451e5a90cbd51792a7496d4e7
Miguel GP <miguel.gironp@gmail.com>
2024-03-11 16:49:46 +0100

    Added data types in Rust

---
+++ data_types/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // numerical data --> list
+    let num = [5, 6, 7, 8];
+
+    // char / string
+    let char = ['a', 'b', 'c', 'd'];
+    let string = "hello world";
+
+    // boolean
+    let boolean = [true, false];
+
+    // list
+    let list = [[1, 2, 3], [4, 5, 6]];
+
+    // dictionary
+    let skills: HashMap<String, i32> = [
+        ("programming".to_string(), 8),
+        ("design".to_string(), 7),
+        ("communication".to_string(), 9),
+    ];
+
+    // tuple
+    let tuple = (1, 2, 3, 4);
+}
